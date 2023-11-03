848e00ad7ae29609fbb61f53c56940eb3b28de9a
haipham2004 <bophamnb2004@gmail.com>
2023-11-03 13:41:20 +0700

    DuAn1_Java

--- src/Demohaa/Demohi.java
+++ src/Demohaa/Demohi.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Demo;
+package Demohaa;
 
 /**
  *
  * @author Admin BVCN88 02
  */
-public class Demohaha {
+public class Demohi {
     
 }
