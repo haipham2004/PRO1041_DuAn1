8279598c5c29cd68aee7385e0491441fb0eb6bb9
NguyenHieu9999 <hieundmph44165@fpt.edu.vn>
2023-11-04 18:53:33 +0700

    Update NhanVienRepository.java

--- src/repository/NhanVienRepository.java
+++ src/repository/NhanVienRepository.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import model.NhanVien;
+import model.TaiKhoan;
 import util.DBConnect1111111;
 
 /**
@@ -17,6 +18,7 @@ import util.DBConnect1111111;
  * @author Admin
  */
 public class NhanVienRepository {
+
     PreparedStatement pst = null;
     Connection conn = null;
     ResultSet rs = null;
@@ -26,11 +28,18 @@ public class NhanVienRepository {
     public List<NhanVien> getAll() {
         try {
             conn = DBConnect1111111.getConnection();
-            sql = "select MaNV, MaTK, HoTen, GioiTinh, DiaChi, SoDienThoai, CCCD, NgayVaoLam, TrangThai, Anh from NhanVien";
+            sql = "select nv.MaNV,nv.MaTK,nv.HoTen,nv.GioiTinh,nv.DiaChi,nv.SoDienThoai,nv.CCCD,nv.NgayVaoLam,nv.TrangThai,nv.Anh,tk.UserName,tk.PassWord,tk.Role,tk.TrangThai \n" +
+"from NhanVien nv join TaiKhoan tk on tk.MaTK = nv.MaTK";
             pst = conn.prepareStatement(sql);
             rs = pst.executeQuery();
             while (rs.next()) {
-                NhanVien nv = new NhanVien(rs.getString(1), rs.getString(2), rs.getString(3), rs.getBoolean(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getDate(8), rs.getBoolean(9), rs.getString(10));
+                TaiKhoan tk = new TaiKhoan(rs.getString(2), rs.getString(11),
+                        rs.getString(12), rs.getString(13), rs.getInt(14));
+                NhanVien nv = new NhanVien(rs.getString(1), tk,
+                        rs.getString(3), rs.getBoolean(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7), rs.getDate(8),
+                        rs.getBoolean(9), rs.getString(10));
+                listNhanVien.add(nv);
             }
         } catch (Exception e) {
             e.printStackTrace();
