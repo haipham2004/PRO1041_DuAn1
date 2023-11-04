90da8687f6eb95d620275859ab87e80c2d825f9b
NguyenHieu9999 <hieundmph44165@fpt.edu.vn>
2023-11-04 18:53:36 +0700

    Create NhanVienServiceImp.java

---
+++ src/service/servicImp/NhanVienServiceImp.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service.servicImp;
+
+import java.util.List;
+import model.NhanVien;
+import repository.NhanVienRepository;
+import service.AdamStore;
+
+/**
+ *
+ * @author Admin
+ */
+public class NhanVienServiceImp implements AdamStore<NhanVien, String>{
+    NhanVienRepository repo = new NhanVienRepository();
+    @Override
+    public List<NhanVien> getAll() {
+        return repo.getAll();
+    }
+
+    @Override
+    public int them(NhanVien k) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int sua(NhanVien k, String e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int xoa(String e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public NhanVien getOne(String e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<NhanVien> getList(String e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
