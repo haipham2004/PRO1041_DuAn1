9bc8f7f36904bd36b17d557c43ae310640ed811c
NguyenHieu9999 <hieundmph44165@fpt.edu.vn>
2023-11-06 13:28:10 +0700

    Update NhanVienRepository.java

--- src/repository/NhanVienRepository.java
+++ src/repository/NhanVienRepository.java
@@ -91,16 +91,66 @@ public class NhanVienRepository {
             return 0;
         }
     }
-    public List<NhanVien> getList(String mot, String hai) {
+    public List<NhanVien> getList2(String mot, String hai) {
         listNhanVien.clear();
         try {
             conn = DBConnect.getConnection();
             sql = "select nv.MaNV,nv.MaTK,nv.HoTen,nv.GioiTinh,nv.DiaChi,nv.SoDienThoai"
                     + ",nv.CCCD,nv.NgayVaoLam,nv.TrangThai,nv.Anh,tk.UserName,tk.PassWord,tk.Role,tk.TrangThai \n"
-                    + "from NhanVien nv join TaiKhoan tk on tk.MaTK = nv.MaTK where ? = ?";
+                    + "from NhanVien nv join TaiKhoan tk on tk.MaTK = nv.MaTK where GioiTinh = ? and DiaChi = ?";
             pst = conn.prepareStatement(sql);
             pst.setObject(1, mot);
             pst.setObject(2, hai);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                TaiKhoan tk = new TaiKhoan(rs.getString(2), rs.getString(11),
+                        rs.getString(12), rs.getString(13), rs.getInt(14));
+                NhanVien nv = new NhanVien(rs.getString(1), tk,
+                        rs.getString(3), rs.getBoolean(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7), rs.getDate(8),
+                        rs.getBoolean(9), rs.getString(10));
+                listNhanVien.add(nv);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return listNhanVien;
+    }
+    public List<NhanVien> getList(String mot) {
+        listNhanVien.clear();
+        try {
+            conn = DBConnect.getConnection();
+            sql = "select nv.MaNV,nv.MaTK,nv.HoTen,nv.GioiTinh,nv.DiaChi,nv.SoDienThoai"
+                    + ",nv.CCCD,nv.NgayVaoLam,nv.TrangThai,nv.Anh,tk.UserName,tk.PassWord,tk.Role,tk.TrangThai \n"
+                    + "from NhanVien nv join TaiKhoan tk on tk.MaTK = nv.MaTK where GioiTinh like ?";
+            pst = conn.prepareStatement(sql);
+            pst.setObject(1, mot);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                TaiKhoan tk = new TaiKhoan(rs.getString(2), rs.getString(11),
+                        rs.getString(12), rs.getString(13), rs.getInt(14));
+                NhanVien nv = new NhanVien(rs.getString(1), tk,
+                        rs.getString(3), rs.getBoolean(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7), rs.getDate(8),
+                        rs.getBoolean(9), rs.getString(10));
+                listNhanVien.add(nv);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return listNhanVien;
+    }
+    public List<NhanVien> getList1(String mot) {
+        listNhanVien.clear();
+        try {
+            conn = DBConnect.getConnection();
+            sql = "select nv.MaNV,nv.MaTK,nv.HoTen,nv.GioiTinh,nv.DiaChi,nv.SoDienThoai"
+                    + ",nv.CCCD,nv.NgayVaoLam,nv.TrangThai,nv.Anh,tk.UserName,tk.PassWord,tk.Role,tk.TrangThai \n"
+                    + "from NhanVien nv join TaiKhoan tk on tk.MaTK = nv.MaTK where DiaChi like ?";
+            pst = conn.prepareStatement(sql);
+            pst.setObject(1, mot);
             rs = pst.executeQuery();
             while (rs.next()) {
                 TaiKhoan tk = new TaiKhoan(rs.getString(2), rs.getString(11),
